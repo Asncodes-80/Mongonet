855af2a1f31325acb95ce184e69003bb5721b196
Asncodes-80 <asn80.asn@hotmail.com>
2021-09-12 12:05:54 +0430

    Mongonet update

---
+++ MongoNet/MongoNet/Controller/GetController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+
+namespace MongoNet
+{
+    class GetController
+    {
+        // Tools method section
+        static DateTime convertToHuman(int timestamp)
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(timestamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+        static void showDetails(CarStream document)
+        {
+            Console.WriteLine($"Doc object ID: {document.Id}");
+            Console.WriteLine($"Plate EN: {document.plate_en}");
+            Console.WriteLine($"Confidence of Capture: {document.confidence}");
+            Console.WriteLine($"Camera ID: {document.camera_id}");
+
+            Console.WriteLine($"Entry vehicle (timestamp): " +
+                $"{document.entry_datetime} To Human DateTime {convertToHuman(timestamp: document.entry_datetime)}");
+
+            Console.WriteLine($"Exit vehicle (timestamp): " +
+                $"{document.exit_datetime} To Human DateTime {convertToHuman(timestamp: document.entry_datetime)} ");
+            Console.WriteLine($"Building: {document.building}");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+        // Main method
+        public void getTrafficsGtTime(string connStr, bool list, int timestamp)
+        {
+            var client = new MongoClient(connStr);
+            // Assume db name is CPR_Paya_DB
+            var db = client.GetDatabase("CPR_Paya_DB");
+            // Assume Table or Collection name is Cars
+            IMongoCollection<CarStream> car_collection = db.GetCollection<CarStream>("Cars");
+
+            var filter = Builders<CarStream>.Filter.Gt("entry_datetime", timestamp);
+            //var tims = car_collection.Find(filter).FirstOrDefault();
+            var cursor = car_collection.Find(filter).ToCursor();
+
+            if (list)
+            {
+                foreach (var document in cursor.ToEnumerable())
+                {
+                    showDetails(document: document);
+                }
+            }
+            else
+            {
+                List<CarStream> cursorLs = cursor.ToList();
+                // Change index if you want first or last of list
+                // 0 => first 
+                // cursorLs.count => last
+                Console.WriteLine($"Length of list: {cursorLs.Count}");
+                Console.WriteLine("------------------------------------");
+                int index = cursorLs.Count - 1;
+                showDetails(document: cursorLs[index]);
+            }
+        }
+
+        public void getOneTraffic(string connStr, int timestamp)
+        {
+            var client = new MongoClient(connStr);
+            // Assume db name is CPR_Paya_DB
+            var db = client.GetDatabase("CPR_Paya_DB");
+            // Assume Table or Collection name is Cars
+            IMongoCollection<CarStream> car_collection = db.GetCollection<CarStream>("Cars");
+
+            var filter = Builders<CarStream>.Filter.Eq("entry_datetime", timestamp);
+            //var tims = car_collection.Find(filter).FirstOrDefault();
+            var cursor = car_collection.Find(filter).ToCursor();
+
+            List<CarStream> cursorLs = cursor.ToList();
+            showDetails(document: cursorLs[0]);
+        }
+    }
+}
---
+++ MongoNet/MongoNet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoNet.Controller;
+
+namespace MongoNet
+{
+    class Program
+    {
+        
+       
+
+        static void Main(string[] args)
+        {
+            var connectionStr = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+            var getControl = new GetController();
+            var delControl = new DeleteController();
+
+            // ATTENTIONS TABLE:
+            // List true ? show list of traffics from your datetime (timestamp) to end : show last traffic;
+            // 1630730919 from 8 days ago to now (day 5) 3 day in total
+
+            //getControl.getTrafficsGtTime(connStr: connectionStr, list: true, timestamp: 1630730919);
+
+            // Getting specific traffic by timestamp
+            //getControl.getOneTraffic(connStr: connectionStr, timestamp: 1630730919);
+
+            // Put your object id
+            //delControl.deleteTrafficByObjID(connStr: connectionStr, objID: "613da5ef39c8be2ac4bc3b8f");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
